ceabafca12e4cfd434351cbcc9e385fd3be7d577
S.Zares <sergiozares@gmail.com>
2018-10-22 22:49:14 +0300

    Add files via upload

---
+++ src/MailerPackage.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace placer\mailer;
+
+use mako\application\Package;
+
+class MailerPackage extends Package
+{
+    /**
+     * Package name.
+     *
+     * @var string
+     */
+    protected $packageName = 'placer/mailer';
+    
+    /**
+     * Package namespace.
+     *
+     * @var string
+     */
+    protected $fileNamespace = 'mailer';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function bootstrap()
+    {
+        $this->container->registerSingleton([Mailer::class, 'mailer'], function()
+        {
+            return new Mailer($this->container);
+        });
+    }
+    
+}
