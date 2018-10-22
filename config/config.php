d500c678974b7563720fb17cf06f92e45900ba13
S.Zares <sergiozares@gmail.com>
2018-10-22 22:44:45 +0300

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,1 +1,27 @@
+<?php
 
+return
+[
+    'settings' =>
+    [
+        'host_name'       => '',
+        'mail_method'     => 'smtp',
+        'smtp_server'     => 'smtp.gmail.com',
+        'smtp_port'       => 587,
+        'smtp_username'   => 'user.name@gmail.com',
+        'smtp_password'   => '******',
+        'smtp_secure'     => true,
+        'from_email'      => 'from.address@gmail.com',
+        'from_name'       => 'From Name',
+    ],
+    'messages' =>
+    [
+        'default' =>
+        [
+            'subject'     => 'Default Message',
+            'view'        => 'mailer::default',
+            'images'      => [],
+            'attachments' => [],
+        ],
+    ],
+];
